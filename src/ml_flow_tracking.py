faa935c4f27a4bcff04ec5d4fc9e181fc81016b6
Arthus Rouhi <arthusrouhi@imac-de-arthus.home>
2023-01-09 17:35:57 +0100

    first commit

---
+++ src/ml_flow_tracking.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import datetime
+datetime.datetime.now()
+import mlflow
+from sklearn.metrics import accuracy_score,precision_score,recall_score
+
+from predict import get_metrics, predictions, run_metrics, classifier
+
+
+def create_experiment(experiment_name,run_name, run_metrics, model, confusion_matrix_path, run_params = None):
+    
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_experiment(experiment_name) 
+    """create experiment"""
+    
+    with mlflow.start_run(run_name=run_name):
+        
+        if not run_params == None:
+            for param in run_params:
+                mlflow.log_param(param, run_params[param])
+            
+        for metric in run_metrics:
+            mlflow.log_metric(metric, run_metrics[metric])
+        
+        
+        if not confusion_matrix_path == None:
+            mlflow.log_artifact(confusion_matrix_path, 'confusion_matrix')
+        
+        """tag the experiment"""
+        mlflow.set_tag("tag1", "Random Forest")
+        
+        """record the model"""
+        mlflow.sklearn.log_model(model, "model")
+        
+
+            
+    print('Run - %s is logged to Experiment - %s' %(run_name, experiment_name))
+    
+    
+date =  datetime.datetime.now().strftime("%Hh%M_%d-%m-%Y")   
+experiment_name = "RDF_classifier"+ date
+run_name="RDF_classifier"+date
+create_experiment(experiment_name, run_name, run_metrics, classifier, '../output/confusion_matrix.png')
--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,6 @@
 from model_training import test_normalized, y_test
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 import datetime
@@ -19,22 +20,27 @@ def get_metrics(y_test, predictions):
             'precision': round(prec, 2), 
             'recall': round(recall, 2)}
 
+def create_confusion_matrix_plot(model, y_test, predictions, path):
+    cm = confusion_matrix(y_test, predictions)
+    print(cm)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.savefig(path)
 
-def export_prediction(data, predict):
+def export_prediction(data, predict, path):
 
     date =  datetime.datetime.now().strftime("%Hh%M_%d-%m-%Y")
-    path = "../output"
 
     index = data.index
     prediction_final = pd.DataFrame({"ID": index, "prediction_label": predict})
 
     prediction_final.to_csv(os.path.join(path, "predictions-"+ date +".csv"))
 
-
+date =  datetime.datetime.now().strftime("%Hh%M_%d-%m-%Y")
 classifier =  pickle.load(open('../model/RDF_classifier.pkl', 'rb'))
 predictions = predict(classifier, test_normalized)
-get_metrics(y_test, predictions)
-cm = confusion_matrix(y_test, predictions)
-print(cm)
+run_metrics = get_metrics(y_test, predictions)
+print(run_metrics)
+create_confusion_matrix_plot(classifier, y_test, predictions, '../output/confusion_matrix.png')
 
-export_prediction(test_normalized, predictions)
+export_prediction(test_normalized, predictions, "../output")
