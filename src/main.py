c236defce8434dc7ced5f6052e3856d5c63d58ac
AR942 <63624017+AR942@users.noreply.github.com>
2023-01-10 19:44:18 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 import pickle
 import datetime
-import os
+from imblearn.under_sampling import RandomUnderSampler
+from collections import Counter
 
 from make_dataset import get_dataframe, index_setting
 from data_preparation import map_function
@@ -13,7 +14,6 @@ from features_engineering import remove_nan, dummies_creation, convert_column
 from model_training import train_test_splitting, normalization_data, model_training
 from predict import predict, get_metrics, create_confusion_matrix_plot, export_prediction
 from ml_flow_tracking import create_experiment 
-
 
 while True:
     modelisation = str(input("Do you want to modelise a train or a test ? :\n"))
@@ -23,36 +23,25 @@ while True:
         break
     except AssertionError:
         pass
-    
-"""while True:
-    modelisation = str(input("Do you want to modelise a train or a test ? :\n"))
-    try:
-        print(os.listdir('../data'))
-        dataset = pd.read_csv("data/{modelisation}.csv")
-        # assert modelisation in ["train", "test"]
-        print(f'You choosed to {modelisation} the model')
-        break
-    except FileNotFoundError:
-        print(f"Console entry has to be either 'train' or 'test'")"""
 
 if modelisation == 'train':
     
     try:
         dataset = get_dataframe("train")
-    except Exception as e:  # TODO Exception should be more precised
+    except Exception as e:
         logging.exception(
             "No data to load : check file path"
         )
     
     index_setting(dataset, "SK_ID_CURR")
     
-    dataset["NAME_TYPE_SUITE"] = map_function(dataset["NAME_TYPE_SUITE"], 5000, "other_NAME_TYPE_SUITE")
-    dataset["NAME_INCOME_TYPE"] = map_function(dataset["NAME_INCOME_TYPE"], 22000, "other_NAME_INCOME_TYPE")
-    dataset["NAME_EDUCATION_TYPE"] = map_function(dataset["NAME_EDUCATION_TYPE"], 5000, "other_NAME_EDUCATION_TYPE")
-    dataset["NAME_FAMILY_STATUS"] = map_function(dataset["NAME_FAMILY_STATUS"], 17000, "other_NAME_FAMILY_STATUS")
-    dataset["NAME_HOUSING_TYPE"] = map_function(dataset["NAME_HOUSING_TYPE"], 10000, "other_NAME_HOUSING_TYPE")
-    dataset["ORGANIZATION_TYPE"] = map_function(dataset["ORGANIZATION_TYPE"], 2500, "other_ORGANIZATION_TYPE")
-    dataset["ORGANIZATION_TYPE"] = map_function(dataset["ORGANIZATION_TYPE"], 9000, "other2_ORGANIZATION_TYPE")
+    dataset.NAME_TYPE_SUITE = map_function(dataset.NAME_TYPE_SUITE, 5000, "other_NAME_TYPE_SUITE")
+    dataset.NAME_INCOME_TYPE = map_function(dataset.NAME_INCOME_TYPE, 22000, "other_NAME_INCOME_TYPE")
+    dataset.NAME_EDUCATION_TYPE = map_function(dataset.NAME_EDUCATION_TYPE, 5000, "other_NAME_EDUCATION_TYPE")
+    dataset.NAME_FAMILY_STATUS = map_function(dataset.NAME_FAMILY_STATUS, 17000, "other_NAME_FAMILY_STATUS")
+    dataset.NAME_HOUSING_TYPE = map_function(dataset.NAME_HOUSING_TYPE, 10000, "other_NAME_HOUSING_TYPE")
+    dataset.ORGANIZATION_TYPE = map_function(dataset.ORGANIZATION_TYPE, 2500, "other_ORGANIZATION_TYPE")
+    dataset.ORGANIZATION_TYPE = map_function(dataset.ORGANIZATION_TYPE, 9000, "other2_ORGANIZATION_TYPE")
     
     dataset_dummies = dummies_creation(dataset)
 
@@ -64,6 +53,7 @@ if modelisation == 'train':
  
     X_train, X_test, y_train, y_test = train_test_splitting(dataset_clean, "TARGET")
 
+    dataset_clean.shape
 
     print(X_train.shape)
     print(y_train.shape)
@@ -73,7 +63,16 @@ if modelisation == 'train':
     train_normalized = normalization_data(1, X_train)
     test_normalized = normalization_data(1, X_test)
     
-    classifier = model_training(RandomForestClassifier(), train_normalized, y_train)
+    """
+    inbalance target class distribution : using undersampling method
+    """
+    rus = RandomUnderSampler() 
+    # undersampling train_normalized, y_train
+    X_rus, y_rus = rus.fit_resample(train_normalized, y_train)
+    # new TARGET class distribution
+    print(Counter(y_rus))
+    
+    classifier = model_training(RandomForestClassifier(), X_rus, y_rus)
     pickle.dump(classifier, open("../model/RDF_classifier.pkl", 'wb'))
     
     date =  datetime.datetime.now().strftime("%Hh%M_%d-%m-%Y")
