7ce9df7b0bb8f18fab312af163a05ed48c23abee
AR942 <63624017+AR942@users.noreply.github.com>
2023-01-17 15:28:48 +0100

    Update ml_flow_tracking.py

--- src/ml_flow_tracking.py
+++ src/ml_flow_tracking.py
@@ -5,6 +5,11 @@ import mlflow
 from sklearn.metrics import accuracy_score,precision_score,recall_score
 
 def create_experiment(experiment_name,run_name, run_metrics, model, confusion_matrix_path):
+    """
+    function pour créer l'experiment mlflow pour pouvoir sauvegarder nos experiences de modélisation
+    """
+    
+    
     """set the localhost url where experiments of mlflow will be hosted"""
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
     mlflow.set_experiment(experiment_name)
